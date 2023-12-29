7dbb80e1d33f707f19ee6f02be9c2d7e2fee53d2
Rion <rion@Rion-MacBook.local>
2023-12-29 15:29:40 +0800

    children bar finish

---
+++ src/views/screen1/components/ChildrenBar.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="bar card">
+    <div class="title">儿童数</div>
+    <ECharts :option="option" width="100%" height="200px" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import ECharts from "@/components/e-charts.vue";
+
+defineProps<{
+  option: object;
+}>();
+</script>
+
+<style lang="scss" scoped>
+.bar {
+  background-image: url("@/assets/children_card.png");
+  width: 831px;
+  height: 247px;
+  margin: 16px auto;
+}
+</style>
--- src/views/screen1/components/ContentMiddle.vue
+++ src/views/screen1/components/ContentMiddle.vue
@@ -1,12 +1,15 @@
 <template>
   <div>
-    <ChildrenPie :count="222" :option="option" :data="childrenData" />
+    <ChildrenPie :count="222" :option="pieOption" :data="childrenData" />
+    <ChildrenBar :option="barOption" />
   </div>
 </template>
 
 <script setup lang="ts">
 import ChildrenPie from "./ChildrenPie.vue";
-const option = {
+import ChildrenBar from "./ChildrenBar.vue";
+
+const pieOption = {
   series: [
     {
       type: "pie",
@@ -82,6 +85,77 @@ const childrenData = [
   { title: "科区", count: 99 },
   { title: "床位", count: 393 },
 ];
+
+const barOption = {
+  grid: {
+    left: 120,
+    top: 20,
+    right: 120,
+    bottom: 30,
+  },
+  xAxis: {
+    type: "category",
+    data: [
+      "<1岁",
+      "1-3岁",
+      "4-5岁",
+      "6-9岁",
+      "10-13岁",
+      "14-15岁",
+      "16-17岁",
+      ">18岁",
+    ],
+    axisLabel: {
+      color: "#00E2FF",
+      fontSize: 14,
+    },
+    axisTick: {
+      show: false,
+    },
+  },
+  yAxis: {
+    type: "value",
+    splitNumber: 5,
+    axisLabel: {
+      color: "#00E2FF",
+      fontSize: 14,
+    },
+    splitLine: {
+      lineStyle: {
+        color: "#00E2FF",
+      },
+    },
+  },
+  series: [
+    {
+      data: [121, 205, 147, 70, 56, 110, 46, 82],
+      type: "bar",
+      barWidth: 30,
+      itemStyle: {
+        color: {
+          type: "linear",
+          x: 0,
+          y: 0,
+          x2: 0,
+          y2: 1,
+          colorStops: [
+            {
+              offset: 0,
+              color: "#00F3FF",
+            },
+            {
+              offset: 1,
+              color: "#00E2FF",
+            },
+          ],
+        },
+        shadowColor: "rgba(255, 255, 255, 0.50)",
+        shadowBlur: 5,
+        opacity: 0.85,
+      },
+    },
+  ],
+};
 </script>
 
 <style lang="scss" scoped></style>
