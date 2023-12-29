eda0ef2064724da4d699d0595f444b05d2e5fb8e
Rion <rion@Rion-MacBook.local>
2023-12-29 17:32:57 +0800

    scale optimize

--- src/utils/scale.ts
+++ src/utils/scale.ts
@@ -6,7 +6,8 @@ export const useScale = () => {
     const clientWidth = document.documentElement.clientWidth;
     if (clientWidth > targetWidth) {
       const scale = clientWidth / targetWidth;
-      document.body.style.cssText = `zoom: ${scale}`;
+      // document.body.style.cssText = `zoom: ${scale}`;
+      document.body.style.cssText = `transform: scale(${scale}); transform-origin: 0 0;`;
     }
   });
 };
