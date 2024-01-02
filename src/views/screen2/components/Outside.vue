4fab8a528b34fd9e9919ebe3f2287c347b8f5231
Rion <rion@Rion-MacBook.local>
2024-01-02 16:38:23 +0800

    outside hospital

---
+++ src/views/screen2/components/Outside.vue
@@ -0,0 +1,184 @@
+<template>
+  <div class="outside card">
+    <div class="title">在外住院 111人</div>
+    <div class="content">
+      <ECharts :option="hospitalOption" width="100%" height="250px" />
+      <Circle ref="circleRef" width="100%" height="280px" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import ECharts from "@/components/e-charts.vue";
+import Circle from "@/components/circle.vue";
+import { onMounted, ref } from "vue";
+
+const hospitalOption = {
+  series: [
+    {
+      type: "pie",
+      radius: "70%",
+      label: {
+        formatter: "{b}\n{c}",
+        fontSize: 14,
+        lineHeight: 20,
+      },
+      labelLine: {
+        length: 20,
+        length2: 30,
+      },
+      data: [
+        {
+          value: 35,
+          name: "和平里医院",
+          itemStyle: {
+            color: "rgba(143, 183, 255, 0.85)",
+            shadowColor: "#5E84C5",
+            shadowBlur: 10,
+          },
+          label: {
+            color: "#9FC1FE",
+          },
+        },
+        {
+          value: 46,
+          name: "李桥医院",
+          itemStyle: {
+            color: "#00FFAC",
+            shadowColor: "rgba(0, 120, 102, 0.57)",
+            shadowBlur: 10,
+          },
+          label: {
+            color: "#00FFAC",
+          },
+        },
+        {
+          value: 19,
+          name: "精保所",
+          itemStyle: {
+            color: "rgba(93, 112, 146, 0.85)",
+            shadowColor: "rgba(23, 37, 57, 0.50)",
+            shadowBlur: 10,
+          },
+          label: {
+            color: "rgba(242, 242, 242, 0.65)",
+          },
+        },
+        {
+          value: 66,
+          name: "火箭军医院",
+          itemStyle: {
+            color: "#F1AD1D",
+            shadowColor: "#A46F00",
+            shadowBlur: 10,
+          },
+          label: {
+            color: "#FFAD00",
+          },
+        },
+        {
+          value: 89,
+          name: "309医院",
+          itemStyle: {
+            color: "#FF876B",
+            shadowColor: "#B3604F",
+            shadowBlur: 10,
+          },
+          label: {
+            color: "#FE9880",
+          },
+        },
+        {
+          value: 89,
+          name: "社福",
+          itemStyle: {
+            color: "rgba(0, 243, 255, 0.85)",
+            shadowColor: "#00AEC0",
+            shadowBlur: 10,
+          },
+          label: {
+            color: "#25EBFE",
+          },
+        },
+        {
+          value: 81,
+          name: "三福",
+          itemStyle: {
+            color: "#FF99C3",
+            shadowColor: "rgba(112, 0, 51, 0.50)",
+            shadowBlur: 10,
+          },
+          label: {
+            color: "#FEA7CB",
+          },
+        },
+      ],
+      emphasis: {
+        itemStyle: {
+          shadowBlur: 10,
+          shadowOffsetX: 0,
+          shadowColor: "rgba(0, 0, 0, 0.5)",
+        },
+      },
+    },
+  ],
+};
+
+const circleRef = ref();
+onMounted(() => {
+  circleRef.value?.run({
+    支气管炎: {
+      $count: 66,
+    },
+    精神发育迟滞: {
+      $count: 23,
+    },
+    肺炎: {
+      $count: 96,
+    },
+    流感: {
+      $count: 79,
+    },
+    兴奋状态: {
+      $count: 16,
+    },
+    上肢骨折: {
+      $count: 20,
+    },
+    下肢骨折: {
+      $count: 9,
+    },
+    流感1: {
+      $count: 79,
+    },
+    兴奋状态2: {
+      $count: 16,
+    },
+    上肢骨折3: {
+      $count: 20,
+    },
+    下肢骨折4: {
+      $count: 9,
+    },
+  });
+});
+</script>
+
+<style lang="scss" scoped>
+.outside {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 532px;
+  height: 598px;
+}
+.title {
+  font-size: 16px;
+  padding: 11px;
+}
+.content {
+  background-image: url("@/assets/outside_background.png");
+  width: 492px;
+  height: 569px;
+}
+</style>
--- src/views/screen2/index.vue
+++ src/views/screen2/index.vue
@@ -1,8 +1,76 @@
-<script setup lang="ts">
-</script>
-
 <template>
-  screen2
+  <div class="background">
+    <div class="overview">
+      <div class="time"></div>
+      <div class="overview-title">儿童概况</div>
+      <div class="time">{{ currentTime }}</div>
+    </div>
+    <div class="content">
+      <div class="content-top">
+        <Outside />
+        <Outside />
+        <Outside />
+      </div>
+    </div>
+  </div>
 </template>
 
-<style scoped></style>
+<script setup lang="ts">
+import { onBeforeUnmount, onMounted, ref } from "vue";
+import { useScale } from "../../utils/scale.ts";
+import dayjs from "dayjs";
+import Outside from "./components/Outside.vue";
+
+useScale();
+const currentTime = ref();
+const getTime = () => {
+  currentTime.value = dayjs().format("YYYY-MM-DD HH:mm:ss");
+};
+
+const intervalId = ref();
+onMounted(() => {
+  intervalId.value = setInterval(getTime, 1000);
+});
+onBeforeUnmount(() => {
+  clearInterval(intervalId.value);
+  intervalId.value = null;
+});
+</script>
+
+<style lang="scss" scoped>
+.background {
+  background-image: url("@/assets/background.jpg");
+  width: 1920px;
+  height: 1080px;
+}
+.overview {
+  background-image: url("@/assets/overview.png");
+  width: 1920px;
+  height: 84px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 10px;
+  &-title {
+    font-weight: 600;
+    color: #ffffff;
+    font-size: 28px;
+  }
+  .time {
+    width: 200px;
+    margin-top: -28px;
+    font-size: 19px;
+    color: #19ecff;
+  }
+}
+.content {
+  background-image: url("@/assets/children_background.png");
+  width: 1890px;
+  height: 996px;
+  margin: 0 auto;
+  &-top {
+    display: flex;
+    justify-content: space-between;
+  }
+}
+</style>
