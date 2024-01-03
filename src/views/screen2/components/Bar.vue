50516e9b86368a78b80782daa277ca59b9729417
Rion <rion@Rion-MacBook.local>
2024-01-03 15:54:50 +0800

    children bar finish

---
+++ src/views/screen2/components/Bar.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="bar">
+    <ECharts :option="option" width="100%" height="100%" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import ECharts from "@/components/e-charts.vue";
+
+defineProps<{
+  option: object;
+}>();
+</script>
+
+<style lang="scss" scoped>
+.bar {
+  background-image: url("@/assets/children_bar.png");
+  align-items: center;
+  justify-content: center;
+  width: 588px;
+  height: 288px;
+}
+</style>
--- src/views/screen2/index.vue
+++ src/views/screen2/index.vue
@@ -11,6 +11,11 @@
         <Statistics :option="pieOption" :statisticsData="statisticsData" />
         <Trend :option="trendOption" :trendData="trendData" />
       </div>
+      <div class="content-bottom">
+        <Bar :option="option1" />
+        <Bar :option="option2" />
+        <Bar :option="option3" />
+      </div>
     </div>
   </div>
 </template>
@@ -22,6 +27,7 @@ import dayjs from "dayjs";
 import Outside from "./components/Outside.vue";
 import Statistics from "./components/Statistics.vue";
 import Trend from "./components/Trend.vue";
+import Bar from "./components/Bar.vue";
 
 useScale();
 const currentTime = ref();
@@ -332,6 +338,481 @@ const trendData = [
     trend: "融合",
   },
 ];
+
+const option1 = {
+  legend: {
+    show: true,
+    top: 10,
+    left: 40,
+    itemWidth: 8,
+    itemHeight: 8,
+    textStyle: {
+      color: "inherit",
+    },
+  },
+  grid: {
+    left: 70,
+    top: 50,
+    right: 70,
+    bottom: 80,
+  },
+  xAxis: {
+    type: "category",
+    data: [
+      "医疗区",
+      "养护一区",
+      "养护二区",
+      "养护三区",
+      "养护四区",
+      "养护五区",
+      "养护六区",
+      "养护七区",
+      "康复师范中心",
+      "特殊教育中心",
+      "救助区",
+    ],
+    axisLabel: {
+      color: "#00E2FF",
+      fontSize: 14,
+      rotate: 45,
+    },
+  },
+  yAxis: {
+    type: "value",
+    splitNumber: 5,
+    axisLabel: {
+      color: "#00E2FF",
+      fontSize: 14,
+    },
+    splitLine: {
+      lineStyle: {
+        color: "#00E2FF",
+      },
+    },
+  },
+  series: [
+    {
+      data: [121, 205, 147, 70, 56, 110, 46, 82, 34, 45, 66],
+      type: "bar",
+      name: "男童",
+      stack: "total",
+      barWidth: 30,
+      itemStyle: {
+        color: {
+          type: "linear",
+          x: 0,
+          y: 0,
+          x2: 0,
+          y2: 1,
+          colorStops: [
+            {
+              offset: 0,
+              color: "#00F3FF",
+            },
+            {
+              offset: 1,
+              color: "#00E2FF",
+            },
+          ],
+        },
+        shadowColor: "rgba(255, 255, 255, 0.50)",
+        shadowBlur: 5,
+        opacity: 0.85,
+      },
+      emphasis: {
+        focus: "series",
+        label: {
+          show: true,
+          color: "#333",
+        },
+      },
+    },
+    {
+      data: [21, 115, 187, 123, 88, 52, 98, 31, 45, 111, 189],
+      type: "bar",
+      name: "女童",
+      stack: "total",
+      barWidth: 30,
+      itemStyle: {
+        color: "#F1AD1D",
+        shadowColor: "#A46F00",
+        shadowBlur: 5,
+        opacity: 0.8,
+      },
+      emphasis: {
+        focus: "series",
+        label: {
+          show: true,
+          color: "#333",
+        },
+      },
+    },
+  ],
+};
+
+const option2 = {
+  legend: {
+    show: true,
+    top: 10,
+    left: 40,
+    itemWidth: 8,
+    itemHeight: 8,
+    textStyle: {
+      color: "inherit",
+    },
+  },
+  grid: {
+    left: 70,
+    top: 50,
+    right: 70,
+    bottom: 80,
+  },
+  xAxis: {
+    type: "category",
+    data: [
+      "医疗区",
+      "养护一区",
+      "养护二区",
+      "养护三区",
+      "养护四区",
+      "养护五区",
+      "养护六区",
+      "养护七区",
+      "康复师范中心",
+      "特殊教育中心",
+      "救助区",
+    ],
+    axisLabel: {
+      color: "#00E2FF",
+      fontSize: 14,
+      rotate: 45,
+    },
+  },
+  yAxis: {
+    type: "value",
+    splitNumber: 5,
+    axisLabel: {
+      color: "#00E2FF",
+      fontSize: 14,
+    },
+    splitLine: {
+      lineStyle: {
+        color: "#00E2FF",
+      },
+    },
+  },
+  series: [
+    {
+      data: [121, 205, 147, 70, 56, 110, 46, 82, 34, 45, 66],
+      type: "bar",
+      name: "0-3岁",
+      stack: "total",
+      barWidth: 30,
+      itemStyle: {
+        color: {
+          type: "linear",
+          x: 0,
+          y: 0,
+          x2: 0,
+          y2: 1,
+          colorStops: [
+            {
+              offset: 0,
+              color: "#00F3FF",
+            },
+            {
+              offset: 1,
+              color: "#00E2FF",
+            },
+          ],
+        },
+        shadowColor: "rgba(255, 255, 255, 0.50)",
+        shadowBlur: 5,
+        opacity: 0.85,
+      },
+      emphasis: {
+        focus: "series",
+        label: {
+          show: true,
+          color: "#333",
+        },
+      },
+    },
+    {
+      data: [21, 115, 187, 123, 88, 52, 98, 31, 45, 111, 189],
+      type: "bar",
+      name: "4-5岁",
+      stack: "total",
+      barWidth: 30,
+      itemStyle: {
+        color: "#F1AD1D",
+        shadowColor: "#A46F00",
+        shadowBlur: 5,
+        opacity: 0.8,
+      },
+      emphasis: {
+        focus: "series",
+        label: {
+          show: true,
+          color: "#333",
+        },
+      },
+    },
+
+    {
+      data: [51, 15, 12, 99, 34, 10, 16, 52, 24, 35, 77],
+      type: "bar",
+      name: "6-13岁",
+      stack: "total",
+      barWidth: 30,
+      itemStyle: {
+        color: "#00FFAC",
+        shadowColor: "rgba(0, 120, 102, 0.57)",
+        shadowBlur: 5,
+        opacity: 0.8,
+      },
+      emphasis: {
+        focus: "series",
+        label: {
+          show: true,
+          color: "#333",
+        },
+      },
+    },
+    {
+      data: [2, 15, 18, 23, 18, 2, 8, 14, 35, 21, 19],
+      type: "bar",
+      name: "14岁以上",
+      stack: "total",
+      barWidth: 30,
+      itemStyle: {
+        color: "#FF99C3",
+        shadowColor: "rgba(112, 0, 51, 0.50)",
+        shadowBlur: 5,
+        opacity: 0.8,
+      },
+      emphasis: {
+        focus: "series",
+        label: {
+          show: true,
+          color: "#333",
+        },
+      },
+    },
+  ],
+};
+
+const option3 = {
+  legend: {
+    show: true,
+    top: 10,
+    left: 40,
+    itemWidth: 8,
+    itemHeight: 8,
+    textStyle: {
+      color: "inherit",
+    },
+  },
+  grid: {
+    left: 70,
+    top: 50,
+    right: 70,
+    bottom: 80,
+  },
+  xAxis: {
+    type: "category",
+    data: [
+      "医疗区",
+      "养护一区",
+      "养护二区",
+      "养护三区",
+      "养护四区",
+      "养护五区",
+      "养护六区",
+      "养护七区",
+      "康复师范中心",
+      "特殊教育中心",
+      "救助区",
+    ],
+    axisLabel: {
+      color: "#00E2FF",
+      fontSize: 14,
+      rotate: 45,
+    },
+  },
+  yAxis: {
+    type: "value",
+    splitNumber: 5,
+    axisLabel: {
+      color: "#00E2FF",
+      fontSize: 14,
+    },
+    splitLine: {
+      lineStyle: {
+        color: "#00E2FF",
+      },
+    },
+  },
+  series: [
+    {
+      data: [21, 25, 17, 7, 5, 10, 4, 18, 24, 15, 6],
+      type: "bar",
+      name: "视力残疾",
+      stack: "total",
+      barWidth: 30,
+      itemStyle: {
+        color: {
+          type: "linear",
+          x: 0,
+          y: 0,
+          x2: 0,
+          y2: 1,
+          colorStops: [
+            {
+              offset: 0,
+              color: "#00F3FF",
+            },
+            {
+              offset: 1,
+              color: "#00E2FF",
+            },
+          ],
+        },
+        shadowColor: "rgba(255, 255, 255, 0.50)",
+        shadowBlur: 5,
+        opacity: 0.85,
+      },
+      emphasis: {
+        focus: "series",
+        label: {
+          show: true,
+          color: "#333",
+        },
+      },
+    },
+    {
+      data: [53, 54, 27, 23, 8, 22, 38, 45, 15, 31, 9],
+      type: "bar",
+      name: "听力残疾",
+      stack: "total",
+      barWidth: 30,
+      itemStyle: {
+        color: "#F1AD1D",
+        shadowColor: "#A46F00",
+        shadowBlur: 5,
+        opacity: 0.8,
+      },
+      emphasis: {
+        focus: "series",
+        label: {
+          show: true,
+          color: "#333",
+        },
+      },
+    },
+
+    {
+      data: [5, 5, 40, 17, 44, 50, 22, 33, 5, 30, 17],
+      type: "bar",
+      name: "言语残疾",
+      stack: "total",
+      barWidth: 30,
+      itemStyle: {
+        color: "#00FFAC",
+        shadowColor: "rgba(0, 120, 102, 0.57)",
+        shadowBlur: 5,
+        opacity: 0.8,
+      },
+      emphasis: {
+        focus: "series",
+        label: {
+          show: true,
+          color: "#333",
+        },
+      },
+    },
+    {
+      data: [12, 16, 22, 76, 23, 12, 33, 14, 65, 11, 69],
+      type: "bar",
+      name: "肢体残疾",
+      stack: "total",
+      barWidth: 30,
+      itemStyle: {
+        color: "#FF99C3",
+        shadowColor: "rgba(112, 0, 51, 0.50)",
+        shadowBlur: 5,
+        opacity: 0.8,
+      },
+      emphasis: {
+        focus: "series",
+        label: {
+          show: true,
+          color: "#333",
+        },
+      },
+    },
+    {
+      data: [52, 34, 22, 76, 23, 12, 28, 14, 65, 11, 69],
+      type: "bar",
+      name: "智力残疾",
+      stack: "total",
+      barWidth: 30,
+      itemStyle: {
+        color: "#D7D7D7",
+        shadowColor: "rgba(15, 33, 60, 0.50)",
+        shadowBlur: 5,
+        opacity: 0.8,
+      },
+      emphasis: {
+        focus: "series",
+        label: {
+          show: true,
+          color: "#333",
+        },
+      },
+    },
+    {
+      data: [42, 16, 77, 76, 23, 12, 18, 14, 65, 11, 69],
+      type: "bar",
+      name: "精神残疾",
+      stack: "total",
+      barWidth: 30,
+      itemStyle: {
+        color: "rgba(255, 135, 107, 0.85)",
+        shadowColor: "#B3604F",
+        shadowBlur: 5,
+        opacity: 0.8,
+      },
+      emphasis: {
+        focus: "series",
+        label: {
+          show: true,
+          color: "#333",
+        },
+      },
+    },
+    {
+      data: [12, 6, 21, 7, 16, 22, 27, 4, 15, 8, 39],
+      type: "bar",
+      name: "多重残疾",
+      stack: "total",
+      barWidth: 30,
+      itemStyle: {
+        color: "#8FB7FF",
+        shadowColor: "#5E84C5",
+        shadowBlur: 5,
+        opacity: 0.8,
+      },
+      emphasis: {
+        focus: "series",
+        label: {
+          show: true,
+          color: "#333",
+        },
+      },
+    },
+  ],
+};
 </script>
 
 <style lang="scss" scoped>
@@ -364,10 +845,22 @@ const trendData = [
   background-image: url("@/assets/children_background.png");
   width: 1890px;
   height: 996px;
+  display: flex;
   margin: 0 auto;
+  flex-direction: column;
+  justify-content: space-between;
   &-top {
     display: flex;
     justify-content: space-between;
+  }
+  &-bottom {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-bottom: 45px;
+    div + div {
+      margin-left: 20px;
+    }
   }
 }
 </style>
